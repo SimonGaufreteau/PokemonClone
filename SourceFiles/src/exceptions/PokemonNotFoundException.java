0a3c6793f165314a08b37050c767fa07565fa33f
Simon <simon.gaufreteau@gmail.com>
2020-01-03 00:01:54 +0100

    Updated PokemonNotFoundException to match integers (id).

--- SourceFiles/src/exceptions/PokemonNotFoundException.java
+++ SourceFiles/src/exceptions/PokemonNotFoundException.java
@@ -5,4 +5,7 @@ public class PokemonNotFoundException extends NotFoundException {
     public PokemonNotFoundException(String name){
         super("Pokemon",name);
     }
+    public PokemonNotFoundException(int id){
+        super("Pokemon",String.valueOf(id));
+    }
 }
