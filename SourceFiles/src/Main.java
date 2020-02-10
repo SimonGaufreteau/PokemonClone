3cea7e1e39d360e60abb3a2d38f830ba195dd62e
Simon <simon.gaufreteau@gmail.com>
2020-02-10 18:44:44 +0100

    Adding a TODO in Square for future advanced corrections (+Checking errors in Main).

--- SourceFiles/src/Main.java
+++ SourceFiles/src/Main.java
@@ -1,4 +1,3 @@
-import entities.Pokemon;
 import pokemonGameUtiles.AbilityDict;
 import pokemonGameUtiles.MoveDict;
 import pokemonGameUtiles.Pokedex;
@@ -20,16 +19,16 @@ public class Main {
             //System.out.println(abilityDict);
 
             MoveDict moveDict = new MoveDict("Moves\\Movelist.txt");
-            //System.out.println(moveDict.getByID(18));
+            System.out.println(moveDict);
 
             Pokedex pokedex = new Pokedex("Pokemons", abilityDict, moveDict);
             //System.out.println(pokedex);
 
-            Pokemon marill = new Pokemon(183, 20, pokedex);
+            /*Pokemon marill = new Pokemon(183, 20, pokedex);
             for (int i = 0; i <= 99; i++) {
                 marill = new Pokemon(183, i, pokedex);
             }
-            System.out.println(marill);
+            System.out.println(marill);*/
         }
         catch (Exception e){
             e.printStackTrace();
