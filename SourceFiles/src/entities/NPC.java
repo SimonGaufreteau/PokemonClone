ae69db17e1dd5ee96ee0e3eb9080fe63847af059
Simon <simon.gaufreteau@gmail.com>
2020-02-10 09:43:26 +0100

    Initializing NPCs

--- SourceFiles/src/entities/NPC.java
+++ SourceFiles/src/entities/NPC.java
@@ -1,4 +1,16 @@
 package entities;
 
 public class NPC {
+    private String name;
+    private int id;
+    private static int countNPC=0;
+
+    public NPC(){
+        this("");
+    }
+
+    public NPC(String name){
+        this.name=name;
+        this.id = countNPC++;
+    }
 }
--- SourceFiles/src/entities/Trainer.java
+++ SourceFiles/src/entities/Trainer.java
@@ -1,4 +1,47 @@
 package entities;
 
-public class Trainer {
+import pokemonGameUtiles.Pokedex;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Trainer extends NPC {
+    private ArrayList<Pokemon> pokemons;
+
+
+    public Trainer(String name, ArrayList<Pokemon> pokemons) {
+        super("Trainer".concat(name));
+        this.pokemons = pokemons;
+    }
+
+    public Trainer(String name, int[] idPokemons, Pokedex pokedex) throws Exception {
+        if (idPokemons.length>6){
+            throw new Exception("Too many ids.");
+        }
+        for (int id:idPokemons) {
+            this.pokemons.add(new Pokemon(id,pokedex));
+        }
+    }
+
+    public Trainer(String name){
+        this(name,new ArrayList<>());
+    }
+
+    public Trainer(){
+        this("");
+    }
+
+
+    public void addPokemon(Pokemon p){
+        this.pokemons.add(p);
+    }
+
+    public ArrayList<Pokemon> getPokemons(){
+        return this.pokemons;
+    }
+
+    public Pokemon getRandomPokemon(){
+        Random r = new Random();
+        return pokemons.get(r.nextInt(pokemons.size()));
+    }
 }
